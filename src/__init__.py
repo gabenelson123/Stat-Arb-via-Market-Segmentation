0ddc5c00a2b605a2b61f46173376bdf826ae1c8b
Gabriel Nelson <gabenelson123@gmail.com>
2025-12-23 11:27:05 -0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Statistical arbitrage research package.
+
+Core pipeline:
+- data loading
+- universe
+- returns
+- factors
+- covariance estimation
+- signal construction
+- portfolio construction
+- residuals
+- backtesting
+"""
